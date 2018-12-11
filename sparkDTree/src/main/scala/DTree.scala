740905a949c3dd19beee10f880b4f6c5e9eabb67
mtalebizadeh <rapex2017@gmail.com>
2018-12-11 08:52:27 -0600

    beforeHyper

--- sparkDTree/src/main/scala/DTree.scala
+++ sparkDTree/src/main/scala/DTree.scala
@@ -1,20 +1,19 @@
 /*
-Including multiple weather station (Sort by Date grouped by station)
-Using different time lags
+Data can be downloaded from the following website:
+http://projects.knmi.nl/klimatologie/daggegevens/selectie.cgi
+
 Calculating different model evaluation metrics
 Parameter tuning using k-fold and  gird search.
 Definition of a base-line model
 For random forest use more than two states for rainfall condition by changing bucketizer parms.
  */
-import org.apache.spark.ml.Pipeline
-import org.apache.spark.sql.{Dataset, Row, SparkSession}
-import org.apache.spark.sql.functions
-import org.apache.spark.sql.Dataset
+import org.apache.spark.sql.{Dataset, Row, SparkSession, functions}
 import org.apache.spark.sql.expressions.Window
-import org.apache.spark.ml.feature.Bucketizer
-import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.{Pipeline, PipelineModel}
+import org.apache.spark.ml.feature.{Bucketizer, VectorAssembler}
 import org.apache.spark.ml.classification.DecisionTreeClassifier
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.tuning.{ParamGridBuilder, TrainValidationSplit}
 
 import scala.util.Random
 object DTree {
@@ -23,39 +22,15 @@ object DTree {
       .appName("DecisionTree")
       .master("local[*]")
       .getOrCreate()
-
     spark.sparkContext.setLogLevel("WARN")
-
     import spark.implicits._
 
+    // Create input dataframe from data file
     val dtInstance = new DTree(spark)
-    val weatherDataFrame = dtInstance.createDataFrame()
-    //weatherDataFrame.show(5,0)
-
-    // Lagging the weatherdataFrame...
-    val window = Window.orderBy("Date")
-      .partitionBy("STN")
-    val laggedDataFrame = weatherDataFrame
-      .withColumn("LagDate", functions.lag($"Date", 1).over(window))
-      .withColumn("LagDDVEC", functions.lag("DDVEC", 1).over(window))
-      .withColumn("LagFHVEC", functions.lag("FHVEC", 1).over(window))
-      .withColumn("LagFG", functions.lag("FG", 1).over(window))
-      .withColumn("LagTG", functions.lag("TG", 1).over(window))
-      .withColumn("LagSQ", functions.lag("SQ", 1).over(window))
-      .withColumn("LagSP", functions.lag("SP", 1).over(window))
-      .withColumn("LagQ", functions.lag("Q", 1).over(window))
-      .withColumn("LagPG", functions.lag("PG", 1).over(window))
-      .withColumn("LagNG", functions.lag("NG", 1).over(window))
-      .withColumn("LagUG", functions.lag("UG", 1).over(window))
-      .withColumn("LagEV24", functions.lag("EV24", 1).over(window))
-      .withColumn("LagRH", functions.lag("RH", 1).over(window))
-      .na.drop()  //Values to assemble cannot be null!
-      // You may drop redundant columns for saving memory
-      .select("LagDate", "LagDDVEC", "LagFHVEC", "LagFG",
-       "LagTG", "LagSQ", "LagSP", "LagQ", "LagPG", "LagNG", "LagUG", "LagEV24",
-       "LagRH", "RH", "Date", "STN")
-
+    val laggedDataFrame = dtInstance.createDataFrame()
     laggedDataFrame.show(10,0)
+
+    // Splits input dataframe to training and test sets
     val Array(trainData, testData) = laggedDataFrame.randomSplit(Array(0.7,0.3))
     trainData.cache()
     testData.cache()
@@ -67,9 +42,8 @@ object DTree {
       .setSplits(Array(Double.NegativeInfinity, 0.1, Double.PositiveInfinity))
     //.transform()
 
-    val countWet = bucketizer.transform(laggedDataFrame).groupBy("WetDry").count()
-
-    countWet.show()
+    val countWetDry = bucketizer.transform(laggedDataFrame).groupBy("WetDry").count()
+    countWetDry.show()
 
 
     // Preparing input vector
@@ -118,6 +92,18 @@ object DTree {
     println(s"Evaluation metric for training data is ${trainEvalMetric} " +
       s"and for test data is ${testEvalMetric}")
 
+    // Tuning hyper parameters
+    val parmGrid = new ParamGridBuilder()
+      .addGrid(decisionTree.)
+
+    val tuningModel = new TrainValidationSplit()
+        .setSeed(Random.nextLong())
+        .setEstimator(pipeline)
+
+
+
+
+
 
 
     trainData.unpersist()
@@ -131,9 +117,7 @@ object DTree {
 class DTree(private val spark: SparkSession) {
 
   import spark.implicits._
-
   def createDataFrame(path: String="/home/mansour/IdeaProjects/predictive_ML/Data/KNMI_20181202_All_Data.txt"): Dataset[Row] = {
-
     val colNames = Seq(
       "STN", "Date","DDVEC", "FHVEC", "FG",
       "FHX", "FHXH", "FHN", "FHNH", "FXX",
@@ -149,13 +133,12 @@ class DTree(private val spark: SparkSession) {
       "STN", "Date", "DDVEC", "FHVEC", "FG", "TG", "SQ",
       "SP", "Q", "PG", "NG", "UG", "EV24", "RH"
     )
-
+    // Loading data from a .csv file
     val weatherData = spark.read
       .option("comment","#")
       .csv(path)
       .toDF(colNames:_*)
       .select(dailyCols.head, dailyCols.tail:_*)
-      //.withColumn("Date", functions.to_date($"Date","yyyymmdd") )
       .withColumn("DDVEC", $"DDVEC".cast("double"))
       .withColumn("FHVEC", $"FHVEC".cast("double"))
       .withColumn("FG", $"FG".cast("double"))
@@ -168,10 +151,55 @@ class DTree(private val spark: SparkSession) {
       .withColumn("UG", $"UG".cast("double"))
       .withColumn("EV24", $"EV24".cast("double"))
       .withColumn("RH", $"RH".cast("double"))
-    //.na.drop()
-    weatherData
+
+    // Lagging weather data and removing redundant columns
+    val window = Window.orderBy("Date")
+      .partitionBy("STN")
+
+    val laggedDataFrame = weatherData
+      .withColumn("LagDate", functions.lag($"Date", 1).over(window))
+      .withColumn("LagDDVEC", functions.lag("DDVEC", 1).over(window))
+      .withColumn("LagFHVEC", functions.lag("FHVEC", 1).over(window))
+      .withColumn("LagFG", functions.lag("FG", 1).over(window))
+      .withColumn("LagTG", functions.lag("TG", 1).over(window))
+      .withColumn("LagSQ", functions.lag("SQ", 1).over(window))
+      .withColumn("LagSP", functions.lag("SP", 1).over(window))
+      .withColumn("LagQ", functions.lag("Q", 1).over(window))
+      .withColumn("LagPG", functions.lag("PG", 1).over(window))
+      .withColumn("LagNG", functions.lag("NG", 1).over(window))
+      .withColumn("LagUG", functions.lag("UG", 1).over(window))
+      .withColumn("LagEV24", functions.lag("EV24", 1).over(window))
+      .withColumn("LagRH", functions.lag("RH", 1).over(window))
+      .na.drop() // feature vales cannot be null!
+      // You may drop redundant columns for saving memory
+      .select("Date", "STN", "LagDate", "LagDDVEC", "LagFHVEC", "LagFG",
+        "LagTG", "LagSQ", "LagSP", "LagQ", "LagPG", "LagNG", "LagUG", "LagEV24",
+        "LagRH", "RH")
+    laggedDataFrame
 
   }
+
+  def dTree(): Unit = {
+    /* save model, show metrics
+    This model makes a binary prediction using the wet threshold value.
+    splitRatios: Array[Double]
+    wetThreshold: Double
+    */
+    ???}
+
+  def dTreeHyperTuned() : Unit = {???}
+
+  def rndForest() : Unit = {
+    /*
+    This model uses rainfall distribution for detemining three or four level of rainfall intensity.
+     */
+
+    ???}
+
+  def rndForestHyperTuned() : Unit = {???}
+
+
+
 
 }
 
